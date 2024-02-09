ada0c79e8516935f91ee758f48da1ba02b765783
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-02-09 20:17:58 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Home from './Pages/Home'; // Import Home component
+import About from './Pages/About'; // Import About component
+import Profile from './Pages/Profile'; // Import Profile component
 
 function App() {
   return (
-    <>
-      <div className='text-red-500'>This is new format</div>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
