5d914a47779e4543314a928b9286447f2fbbe0f8
Ruturaj Deshmukh <ruturajd9696@gmail.com>
2024-02-11 14:53:33 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,20 +1,25 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
-
-// Mongo Database connection succesful !
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Connected to MongoDB!");
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
     console.log(err);
   });
 
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+  console.log('Server is running on port 3000!');
 });
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,13 +11,15 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
